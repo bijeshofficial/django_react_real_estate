e6c4328dd0496a78b58a9cc084dee41269b1e7de
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-09-10 20:19:23 +0545

    tried solving api issue

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,9 @@
-from rest_framework import permissions
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 
 # Create your views here.
 
