5b4f87898149cf8b7a24116e5e11ce3ced67d461
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-09-16 17:30:26 +0545

    started working on frontend

---
+++ frontend/src/containers/Listings.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const listings = () => <div>Listings</div>;
+
+export default home;
---
+++ frontend/src/containers/SingUp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const signUp = () => <div>Sign Up</div>;
+
+export default home;
