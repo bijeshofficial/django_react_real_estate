e8b65501457dcc57bf68e2c6cd41aae5b2276932
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-10-14 20:19:35 +0545

    made changes to scss

---
+++ frontend/build/precache-manifest.7ee14669fc5a6c607271b53d414bbe14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79352fbacd47c6d29ad558b90f76846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e93126399d9289071ca",
+    "url": "/static/css/main.2797cf5d.chunk.css"
+  },
+  {
+    "revision": "a4cdae0e8e3ec35111ed",
+    "url": "/static/js/2.8f0e599b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f0e599b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e93126399d9289071ca",
+    "url": "/static/js/main.7495b190.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
-import { componentWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducers from "./reducers";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducers,
+  rootReducer,
   initialState,
-  componentWithDevTools(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
