fb45038ea6c580e3badf3a20adc2068e8e57c5cf
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-10-16 01:51:03 +0545

    finished with about and contact us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import SignUp from "./containers/SignUp";
 import NotFound from "./components/NotFound";
 
 import Layout from "./hocs/Layout";
+import PrivateRoute from './components/privateRoute';
 
 import "./sass/main.scss";
 
@@ -27,7 +28,7 @@ const App = () => (
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/listings" component={Listings} />
-          <Route exact path="/listings/:id" component={LisitngDetail} />
+          <PrivateRoute exact path="/listings/:id" component={LisitngDetail} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
           <Route component={NotFound} />
--- frontend/src/containers/About.js
+++ frontend/src/containers/About.js
@@ -1,5 +1,149 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import axios from 'axios';
+import House from '../assets/images/house.jpg';
 
-const about = () => <div>About</div>;
+const About = () => {
+    const [topSeller, setTopSeller] = useState([]);
+    const [realtors, setRealtors] = useState([]);
 
-export default about;
+    useEffect(() => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const getTopSeller = async () => {
+            try {
+                const res = await axios.get(`http://localhost:8000/api/realtors/topseller/`, config);
+                setTopSeller(res.data);
+            }
+            catch (err) {
+
+            }
+        };
+
+        getTopSeller();
+    }, []);
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const getRealtors = async () => {
+            try {
+                const res = await axios.get('http://localhost:8000/api/realtors/');
+                setRealtors(res.data);
+            }
+            catch (err) {
+
+            }
+        };
+
+        getRealtors();
+    }, []);
+
+    const getAllRealtors = () => {
+        let allRealtors = [];
+        let results = [];
+
+        realtors.map(realtor => {
+            return allRealtors.push(
+                <Fragment key={realtor.id}>
+                    <div className='about__display'>
+                        <img className='about__display__image' src={realtor.photo} alt='' />
+                    </div>
+                    <h3 className='about__realtor'>{realtor.name}</h3>
+                    <p className='about__contact'>{realtor.phone}</p>
+                    <p className='about__contact'>{realtor.email}</p>
+                    <p className='about__about'>{realtor.description}</p>
+                </Fragment>
+            );
+        });
+
+        for (let i = 0; i < realtors.length; i += 3) {
+            results.push(
+                <div key={i} className='row'>
+                    <div className='col-1-of-3'>
+                        {allRealtors[i]}
+                    </div>
+                    <div className='col-1-of-3'>
+                        {allRealtors[i+1] ? allRealtors[i+1] : null}
+                    </div>
+                    <div className='col-1-of-3'>
+                        {allRealtors[i+2] ? allRealtors[i+2] : null}
+                    </div>
+                </div>
+            );
+        }
+
+        return results;
+    };  
+
+    const getTopSeller = () => {
+        let result = [];
+
+        topSeller.map(seller => {
+            return result.push(
+                <Fragment key={seller.id}>
+                    <div className='about__display'>
+                        <img className='about__display__image' src={seller.photo} alt='' />
+                    </div>
+                    <h3 className='about__topseller'>Top Seller:</h3>
+                    <p className='about__realtor'>{seller.name}</p>
+                    <p className='about__contact'>{seller.phone}</p>
+                    <p className='about__contact'>{seller.email}</p>
+                    <p className='about__about'>{seller.description}</p>
+                </Fragment>
+            );
+        });
+
+        return result;
+    };
+
+    return (
+        <main className='about'>
+            <Helmet>
+                <title>Realest Estate - About</title>
+                <meta
+                    name='description'
+                    content='About us'
+                />
+            </Helmet>
+            <header className='about__header'>
+                <h1 className='about__heading'>About Real Estate</h1>
+            </header>
+            <section className='about__info'>
+                <div className='row'>
+                    <div className='col-3-of-4'>
+                        <h2 className='about__subheading'>No Idea What we are doing</h2>
+                        <p className='about__paragraph'>
+                            Welcome to this project where i only learned about rest framework of django and got lost while doing the frontend. React is confusing as hell right now.
+                        </p>    
+                        <div className='about__display'>
+                            <img className='about__display__image' src={House} alt='' />
+                        </div>
+                        <p className='about__paragraph'>
+                            I Promise to be better at react within the next couple of months.
+                        </p>
+                    </div>
+                    <div className='col-1-of-4'>
+                        {getTopSeller()}
+                    </div>
+                </div>
+            </section>
+            <section className='about__team'>
+                <div className='row'>
+                    <h2 className='about__subheading'>Meet out awesome team!</h2>
+                </div>
+                {getAllRealtors()}
+            </section>
+        </main>
+    );
+};
+
+export default About;
